98c60c41afe46f2756389096c2d4d0c0688d2998
mathews27 <60912700+mathews27@users.noreply.github.com>
2020-02-25 11:01:06 +0530

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,11 @@
+fruits=["apple",'mango']
+numbers=(1,2,3)
+alpha = {'a':'apple','b':'ball'}
+vowels = {'a','e','i','o','u'}
+#list
+print(fruits)
+#tuples
+print(numbers)
+print(alpha)
+print(vowels)
+print("The fruits mentioned are")
